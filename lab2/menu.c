43d5ea977661e454b57494c9a0790b8837d3a8ec
Narglc <qingcheliuzhi@qq.com>
2018-03-15 22:39:22 +0800

    better coding style.

--- lab2/menu.c
+++ lab2/menu.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main(){
   char cmd[5];
   while(1){
     scanf("%s",cmd);
     printf("%s",cmd);
-    if(!strcmp(cmd,"a")){
+    if(strcmp(cmd,"a")==0){
       printf("choice is a\n");
-    }else if(!strcmp(cmd,"b")){
+    }else if(!strcmp(cmd,"b")==0){
       printf("choice is b\n");
-    }else if(!strcmp(cmd,"quit")){
+    }else if(strcmp(cmd,"quit")==0){
       printf("choice is quit\n");
-      break;
+      exit(0);
+    }else{
+      printf("Wrong cmd!\n");
     }
   }
   return 0;
