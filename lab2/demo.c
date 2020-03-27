e854937305d040ded3552fce0b61b1f6def470a8
Narglc Yu <qingcheliuzhi@qq.com>
2020-03-27 23:21:13 +0800

    课后练习

--- lab2/demo.c
+++ lab2/demo.c
@@ -2,15 +2,36 @@
 #include<stdlib.h>
 #include<string.h>
 
+#define PRINTLINE printf("-------------------------------------\n");
+
+void printMenuHelp()
+{
+	PRINTLINE
+	printf("Help Page:\n");
+	printf("1. help 	|	print help info.\n");
+	printf("2. ls   	|	list files cwd\n");	
+	printf("8. quit  	|	quit with 10.\n");
+	PRINTLINE
+}
+
+void lsFilesAtWorkPlace()
+{
+	printf("%s:%s.\n", __FILE__, __FUNCTION__);
+}
+
 int main()
 {
 	char cmd[128];
 	while(1)
 	{
 		scanf("%s",cmd);
-		if(0 == strcmp(cmd,"help"))
+		if(0 == strcmp(cmd, "help"))
 		{
-			printf("This is Menu Help!\n");
+			printMenuHelp();
+		}
+		else if(0 == strcmp(cmd, "ls"))
+		{
+			lsFilesAtWorkPlace();
 		}
 		else if(0 == strcmp(cmd, "quit"))
 		{
