621d6a68ad51950145c7bc4e4a4afd722ad15ef3
Narglc Yu <qingcheliuzhi@qq.com>
2020-03-28 00:29:21 +0800

    逻辑、业务尽量分离

---
+++ lab3/menu3.2.c
@@ -0,0 +1,102 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define PRINTLINE printf("-------------------------------------\n");
+
+#define CMD_MAX_LEN 128
+#define DESC_LEN	10224
+#define CMD_NUM		10
+
+/* data struct and its operations */
+typedef struct DataNode{
+	char*	cmd;
+	char*	desc;
+	int		(*handler)();
+	struct DataNode *next;
+}tDataNode;
+
+tDataNode* FindCmd(tDataNode* head, char* cmd)
+{
+	if(NULL == head || NULL == cmd)
+		return NULL:
+	tDataNode *p = head;
+	while(NULL != p)
+	{
+		if(0 == strcmp(p->cmd, cmd))
+		{
+			return p;
+		}
+		p = p->next;
+	}
+	return NULL;
+}
+
+int ShowAll(tDataNode *head)
+{
+	printf("Menu List:\n");
+	tDataNode *p = head;
+	while(NULL != p)
+	{
+		printf("%s - %s\n", p->cmd, p->desc);
+		p = p->next;
+	}
+	return 0;
+}
+
+int Help();
+int Quit();
+
+
+
+int Help()
+{
+	PRINTLINE
+	printf("Menu List:\n");
+	tDataNode* p = head;
+	while(NULL != p)
+	{
+		printf("%s - %s\n", p->cmd, p->desc);
+		p = p->next;
+	}
+	PRINTLINE
+	return 0;
+}
+
+int Quit()
+{
+	printf("Quit program : [%s:%s].\n", __FILE__, __FUNCTION__);
+	exit(0);
+}
+
+/* menu program */
+static tDataNode head[] = 
+{
+	{"help", 	"this is help cmd!", Help, 	&head[1]},
+	{"version",	"menu program v1.0", NULL,	&head[2]},
+	{"quit",	"Quit from menu",    Quit,	NULL}
+};
+
+int main()
+{
+	/* cmd line begins */
+	while(1)
+	{
+		char cmd[CMD_MAX_LEN];
+		printf("Input a cmd number > ");
+		scanf("%s",cmd);
+		tDataNode *p = FindCmd(head, cmd);
+		if(NULL == p)
+		{
+			printf("This is a wrong cmd!\n");
+			continue;
+		}
+		printf("%s - %s\n", p->cmd, p->desc);
+		if(NULL != p->handler)
+		{
+			PRINTLINE
+			p->handler();
+		}
+	}
+	return 0;
+}
